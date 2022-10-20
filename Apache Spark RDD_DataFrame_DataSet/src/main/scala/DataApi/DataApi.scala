d82bf55a6d44aa42686cb5d7335ad72ca96480c8
Sergei Usovik <41168370+SergUSProject@users.noreply.github.com>
2022-10-20 16:00:43 +0300

    Update DataApi.scala

--- Apache Spark RDD_DataFrame_DataSet/src/main/scala/DataApi/DataApi.scala
+++ Apache Spark RDD_DataFrame_DataSet/src/main/scala/DataApi/DataApi.scala
@@ -1,4 +1,4 @@
-package homework2
+package dataapi
 
 import org.apache.spark.sql.SparkSession
 
@@ -9,16 +9,9 @@ object DataApiHomeWorkTaxi extends App {
     .config("spark.master", "local")
     .getOrCreate()
 
-  val driver = "org.postgresql.Driver"
-  val url = "jdbc:postgresql://localhost:5432/otus"
-  val user = "docker"
-  val password = "docker"
-
-
   val taxiFactsDF = spark.read.load("src/main/resources/data/yellow_taxi_jan_25_2018")
   taxiFactsDF.printSchema()
   println(taxiFactsDF.count())
-
 
 }
 
